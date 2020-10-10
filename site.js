8d4ccf8e6ce52eee3fecb58593d79f8bc68c526f
Safu Dunstan <32492278+Dunstanee@users.noreply.github.com>
2020-10-10 16:14:23 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,74 @@
+function overlay1_on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  function overlay2_on() {
+    document.getElementById("overlay2").style.display = "block";
+  }
+  function overlay3_on() {
+    document.getElementById("overlay3").style.display = "block";
+  }
+  
+  function soff() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay2").style.display = "none";
+    document.getElementById("overlay3").style.display = "none";
+  }
+
+//   end 
+
+function overlay4_on() {
+    document.getElementById("overlay4").style.display = "block";
+  }
+  function overlay5_on() {
+    document.getElementById("overlay5").style.display = "block";
+  }
+  function overlay6_on() {
+    document.getElementById("overlay6").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay4").style.display = "none";
+    document.getElementById("overlay5").style.display = "none";
+    document.getElementById("overlay6").style.display = "none";
+
+  }
+//   end 
+
+//   end 
+
+function overlay7_on() {
+  document.getElementById("overlay7").style.display = "block";
+}
+function overlay8_on() {
+  document.getElementById("overlay8").style.display = "block";
+}
+function overlay9_on() {
+  document.getElementById("overlay9").style.display = "block";
+}
+
+function Poff() {
+  document.getElementById("overlay7").style.display = "none";
+  document.getElementById("overlay8").style.display = "none";
+  document.getElementById("overlay9").style.display = "none";
+
+}
+//   end 
+//   end 
+
+function overlay10_on() {
+  document.getElementById("overlay10").style.display = "block";
+}
+function overlay11_on() {
+  document.getElementById("overlay11").style.display = "block";
+}
+function overlay12_on() {
+  document.getElementById("overlay12").style.display = "block";
+}
+
+function Joff() {
+  document.getElementById("overlay10").style.display = "none";
+  document.getElementById("overlay11").style.display = "none";
+  document.getElementById("overlay12").style.display = "none";
+
+}
+//   end 
